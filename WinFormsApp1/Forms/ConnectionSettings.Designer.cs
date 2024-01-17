e44deb3b4ad343f74cc323629590812f9ef3fd7a
Raphael Raberger <Raphael.Raberger@htl-wels.at>
2024-01-17 18:32:49 +0100

    feat("Added option to set API credentials in the GUI"): API credentials can now be added in the Connection Settings GUI

--- WinFormsApp1/Forms/ConnectionSettings.Designer.cs
+++ WinFormsApp1/Forms/ConnectionSettings.Designer.cs
@@ -32,12 +32,18 @@
             this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 34);
+            this.label1.Location = new System.Drawing.Point(24, 37);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(57, 15);
             this.label1.TabIndex = 0;
@@ -46,7 +52,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 74);
+            this.label2.Location = new System.Drawing.Point(24, 79);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(41, 15);
             this.label2.TabIndex = 1;
@@ -71,12 +77,76 @@
             this.textBox2.UseSystemPasswordChar = true;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(342, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 25);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Get Token";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox3.Location = new System.Drawing.Point(87, 122);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(249, 23);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.UseSystemPasswordChar = true;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox4.Location = new System.Drawing.Point(87, 151);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(249, 23);
+            this.textBox4.TabIndex = 6;
+            this.textBox4.UseSystemPasswordChar = true;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "API Client:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "API Token:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(342, 122);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 52);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Get API Credentials";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // ConnectionSettings
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.ClientSize = new System.Drawing.Size(365, 128);
+            this.ClientSize = new System.Drawing.Size(479, 200);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
@@ -96,5 +166,11 @@
         private Label label2;
         private TextBox textBox1;
         private TextBox textBox2;
+        private Button button1;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Label label3;
+        private Label label4;
+        private Button button2;
     }
 }
--- WinFormsApp1/Forms/ConnectionSettings.cs
+++ WinFormsApp1/Forms/ConnectionSettings.cs
@@ -11,6 +11,11 @@
         {
             textBox1.Text = Settings.model.channel_name;
             textBox2.Text = Settings.model.token;
+
+            textBox3.Text = Settings.model.APIclientID;
+            textBox3.UseSystemPasswordChar = false;
+
+            textBox4.Text = Settings.model.APIsecret;
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -28,6 +33,34 @@
         private void ConnectionSettings_FormClosed(object sender, FormClosedEventArgs e)
         {
             Settings.model.bot = new Bot();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "https://twitchapps.com/tmi/",
+                UseShellExecute = true
+            });
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "https://twitchtokengenerator.com",
+                UseShellExecute = true
+            });
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            Settings.model.APIclientID = textBox3.Text;
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            Settings.model.APIsecret = textBox4.Text;
         }
     }
 }
